2ec2c1ea04d997bb543ad7a51333a1afe232eb2e
joeza247 <mathibela.johannes@outlook.com>
2024-03-26 19:14:00 +0200

    add config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Blacksludge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
